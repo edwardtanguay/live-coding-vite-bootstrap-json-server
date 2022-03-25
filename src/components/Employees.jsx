9a75a07184c8a14d08dc2a6486fb0d2bd22045ec
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-25 14:03:22 +0100

    form with first name

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -42,21 +42,14 @@ export const Employees = () => {
 			</Carousel>
 
 			<Form className="mt-4">
-				<Form.Group className="mb-3" controlId="formBasicEmail">
-					<Form.Label>Email address</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" />
+				<Form.Group className="mb-3">
+					<Form.Label>First Name</Form.Label>
+					<Form.Control type="text"  />
 					<Form.Text className="text-muted">
-						We'll never share your email with anyone else.
+					You can type in an id (<code>/id/</code>) for auto-complete.
 					</Form.Text>
 				</Form.Group>
 
-				<Form.Group className="mb-3" controlId="formBasicPassword">
-					<Form.Label>Password</Form.Label>
-					<Form.Control type="password" placeholder="Password" />
-				</Form.Group>
-				<Form.Group className="mb-3" controlId="formBasicCheckbox">
-					<Form.Check type="checkbox" label="Check me out" />
-				</Form.Group>
 				<Button variant="primary" type="submit">
 					Submit
 				</Button>
