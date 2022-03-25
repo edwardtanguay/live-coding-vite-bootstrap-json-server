39a68a2dcb0287ab52e89767e3bcf68af92088f5
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-25 13:34:59 +0100

    bootstrap is in

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Container, Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Employee Site</h1>
-    </div>
-  )
+	return (
+		<div className="App">
+			<Container>
+				<h1>Employee Site</h1>
+				<Button className="btn-success">Show Info</Button>
+				<Button variant="danger">Delete</Button>
+			</Container>
+		</div>
+	)
 }
 
 export default App
