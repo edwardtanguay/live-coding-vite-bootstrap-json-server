1f9b48d746206cee5bf1cacc057ad6be9510d1ce
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-25 13:49:46 +0100

    make employees component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/bootstrap.superhero.min.css';
 // import './styles/bootstrap.sketchy.min.css';
 import './styles/App.css'
 import { Container, Button } from 'react-bootstrap';
+import { Employees } from './components/Employees';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<h1>Employee Site</h1>
 				<Button className="btn-success">Show Info</Button>
 				<Button variant="danger">Delete</Button>
+				<Employees/>
 			</Container>
 		</div>
 	)
---
+++ src/components/Employees.jsx
@@ -0,0 +1,5 @@
+export const Employees = () => {
+	return (
+		<h2>Employees</h2>
+	)
+}
